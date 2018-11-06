bf85187aa8cabd2bcf3b1952c949eeec1e23bbd5
zhangjw-cmc <44778912+zhangjw-cmc@users.noreply.github.com>
2018-11-07 03:09:35 +0800

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,23 @@
+# 在定义一个类，用来描述听Python的学生
+class PythonStudent():
+    # 用None给不确定的值赋值
+    name = None
+    age = 18
+    course = "Python"
+
+    # 需要注意
+    # 1. def doHomework的缩进层级
+    # 2. 系统默认由一个self参数
+    def DoHomework(self):
+        print("I 在做作业")
+
+        # 推荐在函数末尾使用return语句
+        return None
+
+
+# 实例化一个叫yueyue的学生，是一个具体的人
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.age)
+# 注意成员函数的调用没有传递进入参数
+yueyue.DoHomework()
